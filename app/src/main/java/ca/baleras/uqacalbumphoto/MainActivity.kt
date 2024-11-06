22505756fd113fe9e5289fcbff16588aa2bd4093
Esscraye <esscrayebat@netcourrier.com>
2024-11-06 10:28:08 -0500

    fix image from phone

--- app/src/main/java/ca/baleras/uqacalbumphoto/MainActivity.kt
+++ app/src/main/java/ca/baleras/uqacalbumphoto/MainActivity.kt
@@ -1,29 +1,34 @@
 package ca.baleras.uqacalbumphoto
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import ca.baleras.uqacalbumphoto.ui.theme.UqacAlbumPhotoTheme
 
 class MainActivity : ComponentActivity() {
+    private val REQUEST_CODE_PERMISSIONS = 10
+    private val REQUIRED_PERMISSIONS = arrayOf(
+        android.Manifest.permission.READ_EXTERNAL_STORAGE,
+        android.Manifest.permission.READ_MEDIA_IMAGES
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("TAG", "onCreate: ")
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        Log.d("TAG", "onCreate: allPermissionsGranted")
         setContent {
             UqacAlbumPhotoTheme {
                 val navController = rememberNavController()
-                val devicePhotos = getDevicePhotos(this)
-                val photos = if (devicePhotos.isNotEmpty()) {
-                    devicePhotos
-                } else {
-                    List(200) { "https://picsum.photos/200/300?random=$it" }
-                }
+                val photos = loadPhotos()
                 NavHost(navController = navController, startDestination = "photoGrid") {
                     composable("photoGrid") {
                         PhotoGridScreen(navController, photos)
@@ -33,6 +38,46 @@ class MainActivity : ComponentActivity() {
                         PhotoViewerScreen(photos, index, navController)
                     }
                 }
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        Log.d("TAG", "allPermissionsGranted: ")
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun loadPhotos(): List<String> {
+        Log.d("TAG", "loadPhotos: ")
+        return getDevicePhotos(this)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                setContent {
+                    UqacAlbumPhotoTheme {
+                        val navController = rememberNavController()
+                        val photos = loadPhotos()
+                        NavHost(navController = navController, startDestination = "photoGrid") {
+                            composable("photoGrid") {
+                                PhotoGridScreen(navController, photos)
+                            }
+                            composable("photoViewer/{index}") { backStackEntry ->
+                                val index = backStackEntry.arguments?.getString("index")?.toInt() ?: 0
+                                PhotoViewerScreen(photos, index, navController)
+                            }
+                        }
+                    }
+                }
+            } else {
+                // Handle the case where permissions are not granted
+                List(200) { "https://picsum.photos/200/300?random=$it" }
             }
         }
     }
--- app/src/main/java/ca/baleras/uqacalbumphoto/PhotoUtils.kt
+++ app/src/main/java/ca/baleras/uqacalbumphoto/PhotoUtils.kt
@@ -1,11 +1,13 @@
 package ca.baleras.uqacalbumphoto
 
 import android.content.Context
+import android.net.Uri
 import android.provider.MediaStore
+import android.util.Log
 
 fun getDevicePhotos(context: Context): List<String> {
     val photos = mutableListOf<String>()
-    val projection = arrayOf(MediaStore.Images.Media.DATA)
+    val projection = arrayOf(MediaStore.Images.Media._ID)
     val cursor = context.contentResolver.query(
         MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
         projection,
@@ -14,10 +16,13 @@ fun getDevicePhotos(context: Context): List<String> {
         null
     )
     cursor?.use {
-        val columnIndex = it.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        val columnIndex = it.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
         while (it.moveToNext()) {
-            photos.add(it.getString(columnIndex))
+            val id = it.getLong(columnIndex)
+            val photoUri = Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id.toString())
+            Log.d("getDevicePhotos TAG", "Photo found: $photoUri")
+            photos.add(photoUri.toString())
         }
-    }
+    } ?: Log.d("getDevicePhotos TAG", "Cursor is null")
     return photos
 }
